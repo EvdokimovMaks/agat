210012073f2bc193228ce318b66530e7c5014708
Максим Евдокимов <evdokimovmaks@gmail.com>
2015-08-26 15:18:26 +0300

    build.cmd: теперь чистится лог компилятора; deploy.cmd: добавил возможность добавлять компонент в группы; sets.cmd: исправлены outputresourcepath

--- config/deploy.cmd
+++ config/deploy.cmd
@@ -4,16 +4,17 @@ set ConfigSubDir=%1
 call %ConfigDir%%ConfigSubDir%\sets.cmd
 for /f "delims=" %%x in (version) do set Build=%%x
 
-set Name=%2
-if [%3] == [] (set Resource=%OutputResourceDir%\%~n1%Build%.res) else (set Resource=%3%~n2%Build%.res)
-if [%4] == [] (set Priority=CP_USER) else (set Priority=%4)
-if [%5] == [] (set System=galnet) else (set System=%5)
+set Name=%~2
+if [%~3] == [] (set Resource=%OutputResourceDir%\%~n1%Build%.res) else (set Resource=%~3%~n2%Build%.res)
+if [%~4] == [] (set Group=) else (set Group=%~4)
+if [%~5] == [] (set Priority=CP_USER) else (set Priority=%~5)
+if [%~6] == [] (set System=galnet) else (set System=%~6)
 
 set tmp=%TmpDir%tmp.prj
 set res=%TmpDir%tmp.res
 set lot=%TmpDir%tmp.lot
 
-echo #addcomponent name="%Name%", resource="%Resource%", priority=%Priority%, system="%System%", version="%Build%"; > %tmp%
+echo #addcomponent name="%Name%", resource="%Resource%", priority=%Priority%, system="%System%", version="%Build%", group="%Group%"; > %tmp%
 %CompilerPath% %tmp% /c:"%ConfigDir%vip.cfg" /r:"%res%"
 
 echo update components where (('%Name%' == name)) set version:='%Build%', resource:='%Resource%'; > %lot%
