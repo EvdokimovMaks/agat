5f80127a1dd96a19d6d5de98bfde6ebec9932cbe
Максим Евдокимов <evdokimovmaks@gmail.com>
2015-08-26 15:31:02 +0300

    Докомпилирован интерфейс F_BUHREP::aktreview (ФРО/Отчеты/Журналы-Ордера/Акты сверки): в xlt-отчет добавлена таблица с параметрами отчета Организация и Договор. Изменен xlt-шаблон StartPath/exe/xls/AktReviews_Agat.xlt: в заголовке отображается договор

---
+++ ALTER/deploy-production.cmd
@@ -0,0 +1,1 @@
+@call ../config/deploy.cmd production ArgumentAlter %%%%startpath%%%%_ARGUMENT\ _ARGUMENT
---
+++ ALTER/deploy-test.cmd
@@ -0,0 +1,1 @@
+@call ../config/deploy.cmd test ArgumentAlter %%%%startpath%%%%_ARGUMENT\ _ARGUMENT
